86a2609d3e62707df24517a870fa327f1060b75f
ecoarchie <eco.artur@gmail.com>
2023-02-27 19:45:14 +0300

    fix: cookie with star

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './utils/httpexception.filter';
 
