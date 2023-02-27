2678bf9e58968a6e9163784740e46d63567df8ea
ecoarchie <eco.artur@gmail.com>
2023-02-27 19:33:49 +0300

    cookie

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './utils/httpexception.filter';
 
