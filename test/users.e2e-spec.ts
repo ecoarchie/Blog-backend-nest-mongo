d115a7ce359d079f1070b96e2b5cc89965bda92b
ecoarchie <eco.artur@gmail.com>
2023-03-13 22:57:37 +0300

    test: users blogger route - ban user for blog

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -3,8 +3,9 @@ import { getModelToken, MongooseModule } from '@nestjs/mongoose';
 import { Test } from '@nestjs/testing';
 import { Model, Types } from 'mongoose';
 import request from 'supertest';
+import * as bcrypt from 'bcrypt';
 import { AppModule } from '../src/app.module';
-import { CommentDocument } from '../src/comments/comment-schema';
+import { Blog, BlogDocument, BlogSchema } from '../src/blogs/blog-schema';
 import {
   CreateUserDto,
   User,
@@ -18,7 +19,8 @@ import {
 
 describe('USERS ROUTES', () => {
   let app: INestApplication;
-  let userModel: Model<CommentDocument>;
+  let userModel: Model<UserDocument>;
+  let blogModel: Model<BlogDocument>;
   const ISODateRegex =
     /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)((-(\d{2}):(\d{2})|Z)?)$/;
 
@@ -30,12 +32,17 @@ describe('USERS ROUTES', () => {
             name: User.name,
             schema: UserSchema,
           },
+          {
+            name: Blog.name,
+            schema: BlogSchema,
+          },
         ]),
         AppModule,
       ],
     }).compile();
 
-    userModel = moduleRef.get<Model<CommentDocument>>(getModelToken(User.name));
+    userModel = moduleRef.get<Model<UserDocument>>(getModelToken(User.name));
+    blogModel = moduleRef.get<Model<BlogDocument>>(getModelToken(Blog.name));
 
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe(validationPipeOptions));
@@ -225,6 +232,10 @@ describe('USERS ROUTES', () => {
         userToDelete = await userModel.findOne();
       });
 
+      afterAll(async () => {
+        await userModel.deleteMany({});
+      });
+
       it('should delete user with specified id. Status 204', async () => {
         await request(app.getHttpServer())
           .delete(`/sa/users/${userToDelete.id}`)
@@ -255,5 +266,125 @@ describe('USERS ROUTES', () => {
     });
   });
 
-  describe('Routes for BLOGGERS', () => {});
+  describe('Routes for BLOGGERS', () => {
+    let userToBanUnban: UserDocument;
+    const userDtos: any = [];
+    let users: UserDocument[];
+    let user1: UserDocument;
+    let user2: UserDocument;
+    let validBanInputDto: any;
+    let invalidBanInputDto: any;
+    let accessToken: string;
+    let blog: BlogDocument;
+    beforeAll(async () => {
+      for (let i = 1; i <= 2; i++) {
+        userDtos.push({
+          login: `user ${i}`,
+          email: `user${i}@mail.com`,
+          password: await bcrypt.hash('123456', 1),
+        });
+      }
+
+      await userModel.insertMany(userDtos);
+      users = await userModel.find();
+      user1 = users[0];
+      user2 = users[1];
+
+      const accessTokenRes = await request(app.getHttpServer())
+        .post('/auth/login')
+        .set({ 'user-agent': 'Mozilla' })
+        .send({
+          loginOrEmail: 'user 1',
+          password: '123456',
+        })
+        .expect(200);
+
+      accessToken = accessTokenRes.body.accessToken;
+
+      blog = await blogModel.create({
+        name: 'blog to ban',
+        description: 'description',
+        websiteUrl: 'www.google.com',
+        ownerInfo: {
+          userId: user1._id,
+          userLogin: user1.login,
+        },
+      });
+      validBanInputDto = {
+        isBanned: true,
+        banReason: 'stringstringstringst',
+        blogId: blog.id,
+      };
+      invalidBanInputDto = {
+        isBanned: '',
+        banReason: 'too short',
+        blogId: new Types.ObjectId(),
+      };
+    });
+
+    describe('PUT /blogger/users/{id}/ban - Ban/unban user for blog', () => {
+      afterAll(async () => {
+        await userModel.deleteMany({});
+      });
+
+      it('should ban user if valid inputModel is passed. Status 204', async () => {
+        userToBanUnban = await userModel.findById(user2.id);
+        await request(app.getHttpServer())
+          .put(`/blogger/users/${userToBanUnban.id}/ban`)
+          .set('Authorization', `Bearer ${accessToken}`)
+          .send(validBanInputDto)
+          .expect(204);
+
+        blog = await blogModel.findById(blog.id);
+        expect(blog.bannedUsers).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              id: new Types.ObjectId(userToBanUnban.id),
+            }),
+          ]),
+        );
+        expect(blog.bannedUsers).toHaveLength(1);
+      });
+
+      it('should unban user if valid inputModel is passed. Status 204', async () => {
+        blog = await blogModel.findById(blog.id);
+        expect(blog.bannedUsers[0].id.toString()).toBe(userToBanUnban.id);
+
+        await request(app.getHttpServer())
+          .put(`/blogger/users/${userToBanUnban.id}/ban`)
+          .set('Authorization', `Bearer ${accessToken}`)
+          .send({ ...validBanInputDto, isBanned: false })
+          .expect(204);
+
+        blog = await blogModel.findById(blog.id);
+        expect(blog.bannedUsers[0].banInfo.isBanned).toBe(false);
+      });
+
+      it('should not ban user if invalid inputModel is passed. Status 400', async () => {
+        await request(app.getHttpServer())
+          .put(`/blogger/users/${userToBanUnban.id}/ban`)
+          .set('Authorization', `Bearer ${accessToken}`)
+          .send(invalidBanInputDto)
+          .expect(400);
+      });
+
+      it('should not ban user if blogger is unauthorized. Status 401', async () => {
+        const unauthorizedBloggerToken = 'randomtokenasd;lfkj;asfh;';
+
+        await request(app.getHttpServer())
+          .put(`/blogger/users/${userToBanUnban.id}/ban`)
+          .set('Authorization', `Bearer ${unauthorizedBloggerToken}`)
+          .send(invalidBanInputDto)
+          .expect(401);
+      });
+    });
+
+    describe('GET /blogger/users/blog/{id} - return all banned users for blog', () => {
+      it('should return 5 banned users with pagination. Status 200', async () => {
+        const users = await userModel.find();
+      });
+
+      it('should not return users if admin is unauthorized. Status 401', async () => {});
+    });
+  });
 });
